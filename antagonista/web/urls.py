b58f0d1939abe1cc5aeae0d8e9d848f17bfc5eff
elias gomez fuentes <egomez@informat.cl>
2018-10-29 14:45:30 -0300

    First commit

---
+++ antagonista/web/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'web'
+
+urlpatterns = [
+
+    url(r'^$', views.index_video, name='index_video'),
+    url(r'^inicio$', views.inicio, name='inicio'),
+    url(r'^obras/$', views.obras, name='obras'),
+    url(r'^contacto/$', views.contacto, name='contacto'),
+    url(r'^escuela/$', views.escuela, name='escuela'),
+    url(r'^compania/$', views.compania, name='compania'),
+    url(r'^yo_tambien/$', views.yotambien, name='yotambien'),
+    url(r'^coloquios/$', views.coloquios, name='coloquios'),
+    url(r'^malentendido/$', views.malentendido, name='malentendido'),
+    url(r'^silencio/$', views.silencio, name='silencio'),
+    url(r'^lagaviota/$', views.lagaviota, name='lagaviota'),
+    url(r'^direcciones/$', views.direcciones, name='direcciones')
+
+]
---
+++ antagonista/web/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index_video(request):
+    context = {'message': 'hola'}
+    return render(request, 'index.html', context)
+
+
+def inicio(request):
+    context = {'message': 'hola'}
+    return render(request, 'inicio.html', context)
+
+
+def contacto(request):
+    context = {}
+    print('Estoy en contacto')
+    return render(request, 'contacto.html', context)
+
+def obras(request):
+    context = {}
+    return render(request, 'obras.html', context)
+
+def escuela(request):
+    context = {}
+    return render(request, 'escuela.html', context)
+
+def yotambien(request):
+    context = {}
+    return render(request, 'yo_tambien.html', context)
+
+def coloquios(request):
+    context = {}
+    return render(request, 'coloquios.html', context)
+
+def malentendido(request):
+    context = {}
+    return render(request, 'elmalentendido.html', context)
+
+def silencio(request):
+    context = {}
+    return render(request, 'silencio.html', context)
+
+def direcciones(request):
+    context = {}
+    return render(request, 'direcciones.html', context)
+
+def lagaviota(request):
+    context = {}
+    return render(request, 'lagaviota.html', context)
+
+def compania(request):
+    context = {}
+    return render(request, 'compania.html', context)
